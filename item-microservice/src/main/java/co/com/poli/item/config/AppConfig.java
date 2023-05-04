a9b6eecc70238de2954a4c567e2a5ca819c54f15
juan_montoya <juan_montoya82162@elpoli.edu.co>
2023-05-04 12:16:29 -0500

    Cofiguración de CircuitBreaker de forma programática

--- item-microservice/src/main/java/co/com/poli/item/config/AppConfig.java
+++ item-microservice/src/main/java/co/com/poli/item/config/AppConfig.java
@@ -1,9 +1,16 @@
 package co.com.poli.item.config;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
@@ -13,4 +20,28 @@ public class AppConfig {
     public RestTemplate registerRestTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        //Configuracion para cualquier corto circuito
+        //idCircuitBreaker es el nombre del cortoCircuito
+        //como solo hay uno es el que se ejecuta
+        return resilience4JCircuitBreakerFactory ->
+                resilience4JCircuitBreakerFactory.configureDefault(
+                        idCircuitBreaker -> {
+                            return new Resilience4JConfigBuilder(idCircuitBreaker)
+                                    .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                                            .slidingWindowSize(10)
+                                            .failureRateThreshold(50)
+                                            .waitDurationInOpenState(Duration.ofSeconds(10L))
+                                            .permittedNumberOfCallsInHalfOpenState(5)
+                                            .slowCallRateThreshold(50)
+                                            .slowCallDurationThreshold(Duration.ofSeconds(2L))
+                                            .build())
+                                    .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3L)).build())
+                                    .build();
+                }
+        );
+    }
 }
+
