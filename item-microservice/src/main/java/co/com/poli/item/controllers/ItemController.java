4cc3053040dc82bf07e64c4003f078eba6ca81cf
juan_montoya <juan_montoya82162@elpoli.edu.co>
2023-05-05 12:53:43 -0500

    Add config server and configure Item service in repository

--- item-microservice/src/main/java/co/com/poli/item/controllers/ItemController.java
+++ item-microservice/src/main/java/co/com/poli/item/controllers/ItemController.java
@@ -11,12 +11,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+@RefreshScope
 @RestController
 @RequestMapping("/items")
 public class ItemController {
@@ -24,11 +32,33 @@ public class ItemController {
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
 
     @Autowired
+    private Environment env;
+
+    @Value("${configuracion.texto}")
+    private String textoConfigurationServerConfig;
+
+    @Autowired
     private CircuitBreakerFactory cbFactory;
 
     @Autowired
     @Qualifier(value = "item-service-rest-template")
     private ItemService itemService;
+
+    @GetMapping("/obtenerConfiguracion")
+    public ResponseEntity<?> obtenerConfiguracion(@Value("${server.port}") String port){
+        Map<String, String> json = new HashMap<>();
+        json.put("textoConfigServer", textoConfigurationServerConfig);
+        json.put("puertoConfigServer", port);
+        logger.info("textoConfigServer {}", textoConfigurationServerConfig);
+        logger.info("puertoConfigServer {}", port);
+
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+            json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+    }
 
     @GetMapping
     public List<Item> findAll(
